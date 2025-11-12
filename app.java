e35e09b3cd397a8a83384e6a298969208242b0d1
root <root@ip-172-31-77-29.ec2.internal>
2025-11-12 13:54:09 +0000

    successfully pulled the data

--- app.java
+++ app.java
@@ -3,8 +3,11 @@ public class HelloWorld {
             System.out.println("Hello, World!");
         }
     }
+
 int x = 10;
 int y = 5;
 int sum = x + y; // sum is 15
 double result = (double) x / y; // result is 2.0 (type casting for floating-point division)
 int remainder = x % y; // remainder is 0
+new code by dev 1 with out pulling the code
+>>>>>>> Stashed changes
