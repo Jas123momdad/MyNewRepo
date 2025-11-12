42ed82e80246d6cc9da437baf4b4a9b533f12e2e
Jas123momdad <jasmineshaik317@gmail.com>
2025-11-12 19:03:48 +0530

    More changes app.java

--- app.java
+++ app.java
@@ -3,3 +3,8 @@ public class HelloWorld {
             System.out.println("Hello, World!");
         }
     }
+int x = 10;
+int y = 5;
+int sum = x + y; // sum is 15
+double result = (double) x / y; // result is 2.0 (type casting for floating-point division)
+int remainder = x % y; // remainder is 0
